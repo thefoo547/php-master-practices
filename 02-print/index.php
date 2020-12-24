0a92e75e7adcecbb786071e143b001efad097147
atlasfoo@lin <jf_mejiar547@live.com>
2020-12-24 00:23:02 -0600

    02 print

---
+++ 02-print/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>02-print</title>
+</head>
+<body>
+    <h1>PHP master</h1>
+
+    <?php
+        echo '<h3>Listado de videojuegos</h3>';
+        echo "<ul>"
+            . "<li>GTA</li>"
+            . "<li>Pokemon</li>"
+            . "<li>Zelda</li>"
+            . "</ul>";
+
+        echo '<p>Esto es un parrafo'.' concatenado</p>'
+    ?>
+</body>
+</html>
+
