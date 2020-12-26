a22fb26a8c70b7275f5f78601d48547dcca18c2d
atlasfoo@lin <jf_mejiar547@live.com>
2020-12-26 00:31:01 -0600

    types

---
+++ 04-types/index.php
@@ -0,0 +1,12 @@
+<?php 
+    $integer = 5;
+    $decimal = 2.32;
+    $text = 'text';
+    $bool = true;
+
+    echo gettype($integer);
+    echo gettype($decimal);
+    echo gettype($text);
+    echo gettype($bool);
+
+?>
