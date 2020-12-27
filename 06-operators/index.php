1d65ee84750aedbe48f6f4fdeb646e72cac4c1f7
atlasfoo@lin <jf_mejiar547@live.com>
2020-12-26 22:25:52 -0600

    A

---
+++ 06-operators/index.php
@@ -0,0 +1,10 @@
+<?php 
+    $num1 = 55;
+    $num2 = 33;
+
+    echo 'Suma: '.($num1+$num2).'<br/>';
+    echo 'Resta: '.($num1-$num2).'<br/>';
+    echo 'Multiplicacion: '.($num1*$num2).'<br/>';
+    echo 'Division: '.($num1/$num2).'<br/>';
+    echo 'Mod: '.($num1%$num2).'<br/>';
+?>
